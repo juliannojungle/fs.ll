ac47b77f9f41c8bc3aea37d60254feb37ca63092
Julianno Francisco do Canto Silva <juliannojungle@gmail.com>
2026-07-02 08:14:48 -0300

    💚 fixing simulator build

--- src/lib/Platform/Simulator/HAL.c
+++ src/lib/Platform/Simulator/HAL.c
@@ -1,14 +1,7 @@
 #include "HAL.h"
-#include <stdlib.h>
-#include <SDL2/SDL.h>
+#include <unistd.h>
 
 void Delay(unsigned int milliseconds) {
-    unsigned int start = SDL_GetTicks();
-    SDL_Event event;
-    while (SDL_GetTicks() - start < milliseconds) {
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-            exit(0);
-        SDL_Delay(1);
-    }
+    sleep(milliseconds / 1000);
 }
 void STDIOInitAll(void) {}
