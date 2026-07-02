91c1068f80536c43ac91b5ddc36de93cb847832e
Julianno Francisco do Canto Silva <juliannojungle@gmail.com>
2026-07-02 18:49:22 -0300

    💚 making build system reusable

--- fs.ll.cmake
+++ fs.ll.cmake
@@ -2,23 +2,25 @@ if(NOT DEFINED PLATFORM_NAME)
     set(PLATFORM_NAME "Simulator")
 endif()
 
+if(NOT DEFINED PLATFORM_DIR)
+    set(PLATFORM_DIR "${CMAKE_CURRENT_LIST_DIR}/src/lib/Platform/${PLATFORM_NAME}")
+endif()
+
 set(SOURCES
     ${SOURCES}
     "${CMAKE_CURRENT_LIST_DIR}/src/lib/FileSystem.c"
-    "${CMAKE_CURRENT_LIST_DIR}/src/lib/Platform/${PLATFORM_NAME}/HAL.c"
-    "${CMAKE_CURRENT_LIST_DIR}/src/lib/Platform/${PLATFORM_NAME}/RTC.c"
-    "${CMAKE_CURRENT_LIST_DIR}/src/lib/Platform/${PLATFORM_NAME}/DiskIO.c"
+    "${PLATFORM_DIR}/HAL.c"
+    "${PLATFORM_DIR}/RTC.c"
+    "${PLATFORM_DIR}/DiskIO.c"
     "${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs/source/ff.c"
     "${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs/source/ffsystem.c"
-    "${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs/source/ffunicode.c"
-)
+    "${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs/source/ffunicode.c")
 
 set(INCLUDE_DIRS
     ${INCLUDE_DIRS}
     "${CMAKE_CURRENT_LIST_DIR}/src/lib"
-    "${CMAKE_CURRENT_LIST_DIR}/src/lib/Platform/${PLATFORM_NAME}"
-    "${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs/source"
-)
+    "${PLATFORM_DIR}"
+    "${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs/source")
 
-# Apply patch to ffconf.h (FF_FS_RPATH=1, FF_VOLUMES=2)
+# Apply patch to ffconf.h (FF_FS_RPATH=1, FF_VOLUMES=2, FF_CODE_PAGE=437, FF_USE_LFN=2)
 include(${CMAKE_CURRENT_LIST_DIR}/src/Dependency/fatfs.ffconf_patch.cmake)
--- src/lib/Platform/RP2040/PostExecutable.cmake
+++
@@ -1,8 +0,0 @@
-# target configuration
-set(CMAKE_C_COMPILER /usr/bin/arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-none-eabi-g++)
-
-target_link_libraries(${PROJECT_NAME}
-    PUBLIC
-    pico_stdlib hardware_spi hardware_gpio hardware_adc hardware_rtc
-    fatfs)
